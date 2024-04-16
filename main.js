8b6254abae9ccb464bd3a2afaf5033f8a98f2dc0
baochosoi206 <baochosoi206>
2024-04-16 16:01:19 +0700

    fisrt commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// alert("Chào mừng đến với Web lỏ!");
+// alert("Bấm vào là mất acc:)");
+// var tk = "baobui1";
+// var mk = "09827";
+// let taiKhoan;
+// let matKhau;
+// taiKhoan = prompt("Vui lòng nhập tài khoản");
+// matKhau = prompt("Vui lòng nhập mật khẩu");
+
+// if (taiKhoan === tk && matKhau === mk) {
+//   alert("Đăng nhập thành công");
+// } else {
+//   alert("Tài khoản mật khẩu đã sai");
+//   window.location.href = "https://www.youtube.com/watch?v=PaD4sORO-d8";
+// }
